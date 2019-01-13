9a8a42a2720dff11749f660c03162b141bae8828
koteev <netoxikot@gmail.com>
2019-01-13 18:21:34 +0500

    Найти баланс (задание 6)

---
+++ lesson-02/Task6.java
@@ -0,0 +1,57 @@
+/*      Найти баланс
+
+6. ** Написать метод, в который передается не пустой одномерный целочисленный массив,
+метод должен вернуть true  если в массиве есть место, в котором сумма левой и правой части массива равны.
+
+    Примеры:
+checkBalance([1, 1, 1, || 2, 1]) → true,
+checkBalance ([2,  1,  1, 2,  1]) → false,
+checkBalance ([10, || 10]) → true,
+(граница показана символами ||, эти символы в массив не входят)
+ */
+
+import java.util.Arrays;
+
+public class Task6 {
+    public static void main(String[] args) {
+        test();
+    }
+
+    public static void test() {
+        System.out.println("№6");
+        int[][] input = {
+            {1, 1, 1, 3}, // true
+            {3, 3, 2, 4}, // true
+            {1, 2, 1, 4}, // true
+            {0, 1, 0, 2}, // false
+            {1, 3, 2},    // false
+            {2, 3},       // false
+            {1, 1},       // true
+            {2, 4, 2, 5}, // false
+            {0, 0, 0, 0}, // true
+        };
+
+        for (int[] array: input)
+            System.out.println(Arrays.toString(array) + " - " + checkBalance(array));
+        System.out.println();
+    }
+
+    // Ищет "середину" в массиве и возвращает true, если она найдена
+    public static boolean checkBalance(int[] a) {
+        for (int i = 1; i < a.length; i++) {
+            int[] left = Arrays.copyOfRange(a, 0, i);
+            int[] right = Arrays.copyOfRange(a, i, a.length);
+            if (sum(left) == sum(right))
+                return true;
+        }
+        return false;
+    }
+
+    // Вспомогательная функция - считает сумму элементов массива
+    public static int sum(int[] a) {
+        int sum = 0;
+        for (int num : a)
+            sum += num;
+        return sum;
+    }
+}
