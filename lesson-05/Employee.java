911ffc5b38586aa6e50bf8da1b24863def424bf6
koteev <netoxikot@gmail.com>
2019-01-22 21:38:53 +0500

    Добавил конструктор к классу и изменил модификатор доступа для полей класса

--- lesson-05/Employee.java
+++ lesson-05/Employee.java
@@ -1,9 +1,18 @@
 
 public class Employee {
-    public String name;
-    public int age;
-    public String workpost;
-    public int salary;
-    public String email;
-    public long phone;
+    private String name;
+    private int age;
+    private String workpost;
+    private int salary;
+    private String email;
+    private long phone;
+	
+    public Employee(String name, int age, String workpost, int salary, String email, long phone) {
+        this.name = name;
+        this.age = age;
+        this.workpost = workpost;
+        this.salary = salary;
+        this.email = email;
+        this.phone = phone;
+    }	
 }
