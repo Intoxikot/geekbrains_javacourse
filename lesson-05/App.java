35df8365148c803a3becdbfe5649d9e52e7b9721
koteev <netoxikot@gmail.com>
2019-01-22 21:45:08 +0500

    Добавил вывод на экран сотрудников старше определенного возраста

--- lesson-05/App.java
+++ lesson-05/App.java
@@ -9,5 +9,9 @@ public class App {
         workstate[3] = new Employee("Karl Fridman", 20, "Fractal Engineer", 3400, "fridman@gmail.com", 9909009090L);
         workstate[4] = new Employee("Pavel Evdokimov", 27, "Designer", 2200, "evdokimov@yandex.ru", 9009008080L);
         workstate[5] = new Employee("Sergey Slovakin", 21, "Coffee Department Leader", 4400, "slovakin@yandex.ru", 8006005040L);
+		
+		// В моем штате молодые сотрудники, так что выводим тех, кто от 22 и старше :D
+        for (Employee employee: workstate)
+            if (employee.getAge() >= 22) employee.showInfo();
     }
 }
